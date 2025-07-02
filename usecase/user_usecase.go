d9aadf30477a84faf610bc4923a0ea9c44b7bed2
ryota.hamada <jump1119@icloud.com>
2025-07-03 03:56:51 +0900

    add: SignUp and login API

---
+++ usecase/user_usecase.go
@@ -0,0 +1,61 @@
+package usecase
+
+import (
+	"go-rest-api/model"
+	"go-rest-api/repository"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type IUserUsecase interface {
+	SignUp(user model.User) (model.UserResposen, error)
+	Login(user model.User) (string, error)
+}
+
+type userUsecase struct {
+	ur repository.IUserRepository
+}
+
+func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
+	return &userUsecase{ur}
+}
+
+func (uu *userUsecase) SignUp(user model.User) (model.UserResposen, error){
+	hash, err != bcrypt.GenerateFromPassword([]byte(user.user.Password),10)
+	if err!= nil{
+		return model.UserResposen{}, err
+	}
+	newUser := model.User{Email: user.Email, Password: string(hash)}
+	if err := uu.ur.CreateUser(&newUser); err!=nil{
+		return model.UserResposen{},err
+	}
+	resUser := model.UserResposen{
+		ID: newUser.ID,
+		Email: newUser.Email,
+	}
+	return resUser, nil
+}
+
+func (uu *userUsecase) Login(user model.User) (string, error){
+	storeuser := model.User()
+	if err := uu.ur.GetUserByEmail(&storedUser.Password, user.Email); err := nil {
+		return "",err
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(storeduser.Password), []byte(user.Password))
+	if err!= nil{
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": storeUser.ID,
+		"exp": time.Now().Add(time.Hour * 12).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	if err != nil {
+		return "",err
+	}
+	return tokenString, nil
+}
+
