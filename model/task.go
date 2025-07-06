fad34d0e91bcd8d9c593a423a15166b9c46f6bd5
ryota.hamada <jump1119@icloud.com>
2025-07-06 15:25:12 +0900

    fix: Task model

--- model/task.go
+++ model/task.go
@@ -4,16 +4,16 @@ import "time"
 
 type Task struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"id" gorm:"not null"`
+	Title     string    `json:"title" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"update_at"`
-	User      User      `json:"id" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
+	User      User      `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
 	UserId    uint      `json:"user_id" gorm:"not null"`
 }
 
 type TaksResponse struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"id" gorm:"not null"`
+	Title     string    `json:"title" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"update_at"`
 }
